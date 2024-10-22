dc2b0ea01dff18c6408d77d585edad36be802bcc
Tracy-miranja <barazatracy44@gmail.com>
2024-10-22 14:39:00 +0300

    Add register url

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5173",
   "http://api.amsoljobs.africa",
-  "https://amsoljobs.africa"
+  "https://amsoljobs.africa",
+  " https://amsol-api.onrender.com/api/register "
 ];
 const corsOptions = {
   origin: (origin, callback) => {
